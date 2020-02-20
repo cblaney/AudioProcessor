f2b303b7005c4c6815ae84a651d5d31534f429b3
Chris Blaney <cl.blaney@yahoo.com>
2020-02-20 00:36:17 -0800

    update WavReader comments

--- python/WavReader.py
+++ python/WavReader.py
@@ -19,21 +19,28 @@ class WavReader:
     # Reads in a given wav file
     #
     def __init__(self, filepath):
+        
+        # Read in wav file
         self.sampleRate, self.data = wavfile.read(filepath)
 
-        # Samples are the 0th dimension, Channels are the 1th dimension
-        self.numSamples = np.size(self.data,0)
-        self.numChannels = np.size(self.data,1)
+        # Get number of samples and channels
+        self.numSamples = np.size(self.data,0)  # 0th dimension: Samples
+        self.numChannels = np.size(self.data,1) # 1th dimension: Channels
 
     ## Plots the wav file
     def plot(self):
+
         # Create time vector
         time = np.arange(0, self.numSamples/self.sampleRate, 1/self.sampleRate)
 
         # Plot wavfile data vs time
         plt.figure(1)
-        spn = self.numChannels*100 + 11    # create sub plot number based on number of channels
-        for chan in self.data.T:         # Loop through all channels (transposing data)
+
+        # create sub plot number based on number of channels
+        spn = self.numChannels*100 + 11 
+
+        # Loop through all channels (transposing data)
+        for chan in self.data.T: 
             plt.subplot(spn)        # Create subplot
             plt.plot(time, chan)    # plot the data
             plt.xlabel('time (s)')
