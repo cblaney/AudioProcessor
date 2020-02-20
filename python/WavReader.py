4e3796f9674066242f1cdfffa526e021c1afd0b8
Chris Blaney <cl.blaney@yahoo.com>
2020-02-20 00:54:37 -0800

    add WavReader::getMono

--- python/WavReader.py
+++ python/WavReader.py
@@ -5,6 +5,7 @@
 #
 
 import matplotlib.pyplot as plt
+from matplotlib import interactive
 import numpy as np
 from scipy.io import wavfile
 
@@ -27,14 +28,22 @@ class WavReader:
         self.numSamples = np.size(self.data,0)  # 0th dimension: Samples
         self.numChannels = np.size(self.data,1) # 1th dimension: Channels
 
+
+    ## Returns wav data as single channel audio
+    def getMono(self):
+        
+        # return the normalized sum across the channels
+        return np.sum(self.data, axis=1)/self.numChannels
+
+
     ## Plots the wav file
     def plot(self):
 
         # Create time vector
         time = np.arange(0, self.numSamples/self.sampleRate, 1/self.sampleRate)
 
-        # Plot wavfile data vs time
-        plt.figure(1)
+        # Create a new figure
+        plt.figure()
 
         # create sub plot number based on number of channels
         spn = self.numChannels*100 + 11 
@@ -47,11 +56,12 @@ class WavReader:
             plt.ylabel('amplitude')
             spn = spn+1             # increase subplot number
         
-        # display wavfile
-        plt.show()
-
     
 # Test code
+interactive(True)
 wr = WavReader('./../audio/c_chord.wav')
+plt.figure()
+plt.plot(wr.getMono())
 wr.plot()
+input('press return to continue')
 
